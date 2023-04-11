192e2940548c526b68eb55de64b1d77b9ade8397
palani <palanibaba2002@gmail.com>
2023-04-11 08:53:57 +0530

    Add character images in cartoons axios

--- src/Components/Routers/Cartoons/Cartoons.jsx
+++ src/Components/Routers/Cartoons/Cartoons.jsx
@@ -23,19 +23,21 @@ export class Cartoons extends Component {
     const {character}=this.state
     return (
       <React.Fragment>
-          <div className='title'>The Rick and Morty API</div>
+          <div className='title'><h1>The Rick and Morty API</h1></div>
         <div className='container'>
         {character.length
-        ?character.map(character=><div>{character.name}<br/> 
-        {character.image} <br/>
-        {character.status} <br/>
-        {character.species} <br/>
-        {character.type} 
+        ?character.map(character=><div className='content'>
+          <img src={character.image} alt="" />
+       <p>Name: {character.name} </p>
+        <p>Status: {character.status} <br/> </p>
+        <p>Species: {character.species} <br/></p>
         {character.gender} 
         <p></p>
           </div>):null}
         <h1>Cartoons</h1>
       </div>
+    
+  
       </React.Fragment>
     )
   }
